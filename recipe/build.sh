7e4e9032dcea0cd7724c90c56f2e273328e788c1
Ray Donnelly <mingw.android@gmail.com>
2020-09-28 01:24:17 +0200

    Some fixes for macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-if [[ $(uname) == Darwin ]]; then
-  export CC=clang
-  export CXX=clang++
-  export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib -headerpad_max_install_names $LDFLAGS"
-  export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
-  export MACOSX_DEPLOYMENT_TARGET="10.9"
-  export CXXFLAGS="-stdlib=libc++ $CXXFLAGS"
-fi
-
 cd build_unix
-../dist/configure --prefix=$PREFIX \
-                  --enable-shared \
-                  --disable-static \
-                  --enable-cxx \
-                  --enable-stl
+../dist/configure \
+  --prefix=$PREFIX \
+  --host=${HOST} \
+  --enable-shared \
+  --disable-static \
+  --enable-cxx \
+  --enable-stl
 
 make -j$CPU_COUNT
 make check -j$CPU_COUNT
